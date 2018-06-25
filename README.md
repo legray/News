5a210a1ea1b4e137477cda49a637af766a3a7df3
legray <40530151+legray@users.noreply.github.com>
2018-06-25 11:40:55 -0400

    Add files via upload
    Added newsdata.py, README.md and newsdata.zip which contains the newsdata.sql file.  Unable to push newsdata.sql because file was too large.  Needed to be condensed to a zip file.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+Please NOTE:  This project was developed on a Windows 10 Laptop.
+Due to a known bug in Windows 10, Hashicorp, makers of Vagrant has confirmed that there is a bug, but no bug fix.
+As a work around to this issue and not being able to run Vagrant and Virtual Box properly,
+the project was developed using Postgresql and PgAdmin 4.
+You will need to install and run Postgresql and PgAdmin4 on your computer.
+
+copy this README.md and Newsdata.py files for this project in a directory under Downloads called News
+where you should also have the newsdata.sql file for this project installed.
+Open your command prompt and switch to SSH and postgreSQL
+change directory to downloads/News and in python run newsdata.py
+
+The following files should be used for this project,  README.md, newsdata.py and newsdata.sql file provided
+from the Logs Analysis assignment
+
+Access via the command line using psql command
+log in with your postgres password
+\l (to view the available databases, news should be one of the dbases set up.)
+\c news (to switch to the news database)
+news-# prompt should appear
+
+# News
 # News
---
+++ newsdata.py
@@ -0,0 +1,64 @@
+#This script will run 3 seperate queries from the News DB and it's 3 seperate
+#tables, called
+#articles, authors and log
+#This script assumes that you will be running the python script in
+#SSH from postgreSQL.
+#Make sure your Newsdata SQL files are setup and installed in postgreSQL.
+#
+
+import psycopg2
+
+# Connect to an existing database
+conn = psycopg2.connect("dbname=news user=postgres")
+
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+#New Command, Executes and prints out results of question 1 in Logs
+#Analysis project.
+cur.execute (
+    "SELECT articles.title AS 'Articles_Name'"
+    "COUNT (log.time) AS 'Views'"
+    "FROM articles, log"
+    "WHERE substring(log.path.10) = articles.slug"
+    "GROUP BY articles.title"
+    "ORDER BY Views DESC LIMIT 3"
+          )
+print cur.fetchall()
+
+print(results)
+
+#New Command, Executes and prints out results of question 2 in Logs
+#Analysis project.
+cur.execute (
+    "SELECT authors.name AS 'Authors_Name', COUNT (log.time) AS 'Views'"
+    "FROM authors, articles, log"
+    "WHERE articles.author = authors.id"
+    "GROUP BY authors.name"
+    "ORDER BY Views DESC;"
+           )
+print cur.fetchall()
+
+print(results)
+
+#New Command, Executes and prints out results of question 3 in Logs
+#Analysis project.
+cur.execute (
+    "SELECT *"
+        "FROM ("
+            "SELECT to_char(time,'MonthDD, YYYY') as date,"
+                "(COUNT(status) filter"
+                    "(WHERE status LIKE '404%')*100.0/COUNT(status)"
+                ")"
+        ") as errors"
+    "FROM log"
+    "GROUP BY date) as errors_table"
+    "WHERE errors_table.errors > 1.0"
+    )
+
+print cur.fetchall()
+                
+print(results)
+
+db.close()
